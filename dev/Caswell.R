8122a9106e10116dce9c182524910c7e98e4fc67
Tim Riffe <tim.riffe@gmail.com>
2019-06-19 13:56:55 +0200

    ltre now documented, but needs examples, and maybe comparisons.

--- dev/Caswell.R
+++ dev/Caswell.R
@@ -11,38 +11,34 @@
 #' \code{pars1} warps into \code{pars2} over \code{N} steps. This allows for arbitrary precision as 
 #' \code{N} increases, as in the case of the Horiuchi approach. 
 #' 
-#' @note The case of \code{N=1} differentiates with respect to the arithmetic mean of \code{pars1}
-#'  and \code{pars2}. The \code{...} argument can be used to send extra parameters to \code{func()} 
-#' that do not get decomposed, or to specify other optional arguments to \code{numDeriv::grad()} 
-#' for finer control. 
+#' @details The case of \code{N=1} differentiates with respect to the arithmetic mean of \code{pars1} and \code{pars2}. The \code{...} argument can be used to send extra parameters to \code{func()} that do not get decomposed, or to specify other optional arguments to \code{numDeriv::grad()} for finer control. 
+#' 
+#' The argument \code{dfunc} is optional. If given, it should be a function written to have a first argument \code{func}, a second argument \code{x}, which consists in the vector of decomposed parameters (same layout at \code{pars1} and \code{pars2}), and an option \code{...} argument for undecomposed parameters. Presumably if a derivative function is given then it is analytic or somehow a more parsimonious calculation than numeric derivatives. If left unspecified \code{numDeriv::grad()} is used.
+#' 
+#' As with \code{horiuchi()}, the path from \code{pars1} to \code{pars2} is linear, but other paths can be induced by parameterizing \code{func()} differently. For example, if you want proportional change from \code{pars1} to \code{pars2} then log them, and write \code{func()} to first antilogging. This is not zero-friendly, but in practice power transforms give close results, so you could \code{sqrt()} and then square inside \code{func()}. If you do this, then \code{dfunc()} must be written to account for it too, or you could stick with the default numeric gradient function.
+#' 
 #' @importFrom numDeriv grad
 #' @seealso \code{\link[numDeriv]{grad}}
 #' @inheritParams horiuchi
 #' @param ... \dots optional parameters to pass on to \code{func()}. These are not decomposed. Also one can use this argument to pass optional arguments to \code{numDeriv::grad()}.
+#' @param dfunc a derivative function, see details
 #' @export
 #' @references 
 #' \insertRef{caswell1989analysis}{DemoDecomp}
 #' \insertRef{caswell2006matrix}{DemoDecomp}
 
 ltre <- function(func, pars1, pars2, dfunc, N = 20, ...){
-	if (missing(dfunc)){
+	if (missing(dfunc)){ 
 		dfunc <- numDeriv::grad
 	}
+	stopifnot(is.function(dfunc))
+    stopifnot(length(pars1) == length(pars2))
 	
-	# what if user gives vector or matrix of derivatives?
-	# then presumably the derivatives were taken along the 
-	# same path from pars1 to pars2!!! 
-#	numd <- FALSE
-#	if (!is.function(dfunc)){
-#		if (is.vector(dfunc) | is.matrix(dfunc)){
-#			stopifnot( length(dfunc) %% N == 0 )
-#			der   <- matrix(dfunc, ncol = N)
-#			numd  <- TRUE
-#		}
-#	}
+	
 	delta       <- pars2 - pars1
 	n 			<- length(pars1)
 	ddelta 		<- delta / N
+	
 	x           <- pars1 + d * matrix(rep(.5:(N - .5) / N, n), 
 			                          byrow = TRUE, 
 									  ncol = N)
