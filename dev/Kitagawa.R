9ee454652bb59eea3b090d52fe8b58bdbb9c6485
Tim Riffe <tim.riffe@gmail.com>
2019-08-18 16:35:46 -0400

    shell w notes on kitagawa das gupta

---
+++ dev/Kitagawa.R
@@ -0,0 +1,19 @@
+
+# Author: tim
+###############################################################################
+
+# I never really thought of the Kitagawa method as generalizable until now. Here goes:
+
+#kitagawa <- function(func, pars1, pars2){
+#	
+#}
+## hmm oh ya this one assumes we have equal-length vectors of arguments, 
+# in the first instance two vectors of args, but Das Gupta generalized up to 5 or 6 vectors.
+# Would be nice to generalize to N vectors. Still the method would only be useful for functions
+# only consisting in equal-length vectors of arguments, no other solitary parameters. 
+
+# so in a sense, func() would want to take a single matrix as its argument rather than a vector.
+# that way the number of parameters is at least established in a general way.
+
+
+
